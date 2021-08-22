454d1c8bc8bdb17b0e481ec7bdfc595b70407c56
Demilululu <demilululu@gmail.com>
2021-08-23 00:09:30 +0800

    Add express-session and login feature via passport

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
 const bcrypt = require('bcryptjs')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const usePassport = require('./config/passport')
 
 const db = require('./models')
 const User = db.User
@@ -12,8 +15,16 @@ const Todo = db.Todo
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 
 // Index page
 app.get('/', (req, res) => {
@@ -28,38 +39,62 @@ app.get('/', (req, res) => {
 // Detail page
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
-  
+
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
 
-
+// Login
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+
+// Register
 app.get('/users/register', (req, res) => {
   res.render('register')
 })
 
 app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  console.log(name)
-  console.log(email)
-  console.log(password)
-
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  // check email 
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        console.log('User already exists')
+        return res.render('register', {
+          name, email, password, confirmPassword
+        })
+      }
+    })
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(password, salt))
+    .then(hash => {
+      User.create({ name, email, password: hash })
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
+// New Todo
 app.get('todos/new', (req, res) => {
   res.render('new')
 })
 
+// Edit Todo
 app.get('todos/edit', (req, res) => {
   res.render('edit')
 })
+
+//Delete Todo
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
