d6365a1f24f4aca39d8006a87dc4a0c8b7b00234
Demilululu <demilululu@gmail.com>
2021-08-23 20:32:33 +0800

    Add register error notice

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -14,25 +14,36 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符。' })
+  }
+
+  if (errors.length) {
+    return res.render('register', { errors, name, email, password, confirmPassword })
+  }
 
   // check email 
   User.findOne({ where: { email } })
     .then(user => {
       if (user) {
-        console.log('User already exists')
-        return res.render('register', {
-          name, email, password, confirmPassword
-        })
+        errors.push({ message: '這個Email已經註冊過了。' })
+        return res.render('register', { name, email, password, confirmPassword, errors })
       }
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => {
+          User.create({ name, email, password: hash })
+        })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
     })
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(password, salt))
-    .then(hash => {
-      User.create({ name, email, password: hash })
-    })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
 })
 
 // Login
